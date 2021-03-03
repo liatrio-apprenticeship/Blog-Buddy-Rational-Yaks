31a1e13dd7c676e09177a50d6f673882b8246d91
Samantha Mathistad <ssmathistad@mail.csuchico.edu>
2021-03-03 09:27:39 -0800

    Adding support for 'hears' trigger for blog title

--- bot_vol/features/blog.js
+++ bot_vol/features/blog.js
@@ -1,10 +1,21 @@
 module.exports = function(controller) {
-    controller.hears(new RegExp(/^blog filter author (.*?)$/i), 'message', async(bot, message) => {
-        
-        let sql = `SELECT author FROM blogs`;
+    controller.hears(new RegExp(/^bb filter title (.*?)$/i), 'message', async(bot, message) => {
+
+        let param = message.matches[1];
+
+        let sql = `SELECT author, title, link_liatrio FROM blogs WHERE title LIKE \'%${ param }%\'`;
+
         const result = await controller.plugins.db.all(sql);
 
-        console.log(results);
+        console.log(result);
+
+        let response = "";
+
+        for (let i = 0; i < result.length; i++) {
+            response += result[i].author + " - " + result[i].title + " " + result[i].link_liatrio + "\n";
+        }
+
+        await bot.reply(message, "Query:\n" + response);
     
-    }
+    });
 }
