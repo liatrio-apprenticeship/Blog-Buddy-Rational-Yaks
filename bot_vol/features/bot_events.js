57cc9ad6caaf4a4647ee5146cfbebc9f22d157c8
Connor Mulligan <connorm@Connors-MacBook-Pro.local>
2021-03-04 17:11:25 -0800

    Added bot help command

--- bot_vol/features/bot_events.js
+++ bot_vol/features/bot_events.js
@@ -1,47 +1,99 @@
 module.exports = function(controller) {
-    controller.hears(['help', 'help me', 'need assistance!', 'save me!!!'], 'message', async(bot, message) => {
-        await bot.reply(message,{
-            blocks: [
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": "Hello! This is Blog Buddy :) - Here's a list of of commands I can execute."
-                    }
-                },
-                {
-                    "type": "divider"
-                },
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": "Usage: /blog-help <COMMAND> [OPTIONS]\n"
-                    }
-                },
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": "list - Displays all up to date blogs written by Liatrio employees.\n" //change
-                    }
-                },
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": "list - Displays all up to date blogs written by Liatrio employees.\n" //change
-                    }
-                },
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": "list - Displays all up to date blogs written by Liatrio employees.\n"
-                    }
-                },
-                
-            ]
+	controller.hears(['help', 'help me', 'need assistance!', 'save me!!!'], 'message', async(bot, message) => {
+		await bot.reply(message,{
+			blocks: [
+					{
+						"type": "section",
+						"text": {
+							"type": "mrkdwn",
+							"text": "Hello, Blog Buddy at your service :nerd_face: - Here's a list of of commands I can execute."
+						}
+					},
+					{
+						"type": "divider"
+					},
+					{
+						"type": "divider"
+					},
+					{
+						"type": "section",
+						"text": {
+							"type": "mrkdwn",
+							"text": "`Usage:` bb <COMMAND> [OPTION]\n\n"
+						}
+					},
+					{
+						"type": "divider"
+					},
+					{
+						"type": "divider"
+					},
+
+					{
+						"type": "section",
+						"text": {
+							"type": "mrkdwn",
+							"text": `*Commands:* \n`
+						}
+					},
+
+					  
+					{
+						"type": "section",
+						"text": {
+							"type": "mrkdwn",
+							"text": "`list` - Displays all up to date blogs written by Liatrio team members.\n" //change
+						}
+					},
+					{
+						"type": "section",
+						"text": {
+							"type": "mrkdwn",
+							"text": "`filter [OPTIONS]` - Based on specified options, filter will return the specified crtieria. i.e. filter author <author_name> will return blogs written by that author.\n\n" //change
+						}
+					},
+					{
+						"type": "divider"
+					},
+					{
+						"type": "divider"
+					},
+					{
+						"type": "section",
+						"text": {
+							"type": "mrkdwn",
+							"text": `*Options:* \n`
+						}
+					},
+					{
+						"type": "section",
+						"text": {
+							"type": "mrkdwn",
+							"text": "`author` - Displays all blogs written by a certain author.\n"
+						}
+					},
+					{
+						"type": "section",
+						"text": {
+							"type": "mrkdwn",
+							"text": "`title` - Displays all blogs containing a specified title.\n"
+						}
+					},
+					{
+						"type": "section",
+						"text": {
+							"type": "mrkdwn",
+							"text": "`random` - Returns a randomly selected blog.\n"
+						}
+					},
+					{
+						"type": "divider"
+					},
+					{
+						"type": "divider"
+					},
+			
+		    ]
         })
     });
 }
